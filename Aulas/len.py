162c58e52a491f83d64c15902da93341127e9dfc
Amanda Couto <mandscouto@outlook.com>
2025-09-28 17:13:16 -0300

    estudando

---
+++ Aulas/len.py
@@ -0,0 +1,11 @@
+#Retorna a quantidade de elementos de uma lista/tupla/dicionario
+
+listaExemplo = [1, 2, 3, 4, 5, 6, 7, 8, 8, 10]
+dicionarioExemplo = {'Nome' : 'Amanda', 'Idade' : 23}
+tuplaExemplo =  (1, 2, 3)
+stringExemplo = 'Olá, mundo!'
+
+print(len(listaExemplo))
+print(len(dicionarioExemplo))
+print(len(tuplaExemplo))
+print(len(stringExemplo))
---
+++ Aulas/round.py
@@ -0,0 +1,5 @@
+#Comando round usado para arredondar o valor usado
+
+valorExemplo = 12.1233456563321
+valorExemplo = round(valorExemplo,3)
+print(valorExemplo)
