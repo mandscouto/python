d341b4c6a6f2d9f533a5d849e9dade8ca2ba8580
Amanda Couto <mandscouto@outlook.com>
2025-09-28 21:04:31 -0300

    estudando

---
+++ Aulas/funcao.py
@@ -0,0 +1,22 @@
+def Boas_Vindas():
+    print('PYTHON')
+
+Boas_Vindas()
+
+######################
+
+def Somar(valor1, valor2):
+    soma= valor1 + valor2
+    print(soma)
+
+Somar (5, 6)
+
+#######################
+
+for loop in range(0, 10):
+    import random
+
+    x = random.random()
+    y = random.random()
+
+    Somar(x, y)
