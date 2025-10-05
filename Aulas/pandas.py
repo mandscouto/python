56bc86fce309d6692d80952b213d0d53b7f52a1f
Amanda Couto <mandscouto@outlook.com>
2025-10-05 16:36:14 -0300

    estudando

--- Aulas/pandas.py
+++ Aulas/pandas.py
@@ -1,15 +1,1 @@
-import pandas as pd
-
-df = pd.read_csv('StudentsPerformance.csv')
-
-type (df)
-
-df.head() #Exibe as 5 primeiras linhas
-
-df.tail() #Exibe as 5 últimas linhas
-
-df.shape #Exibe a quantidade de linhas e colunas
-
-df.columns #Exibe um array com o nome das colunas
-
-df.duplicated().sum() #Verifica linhas duplicadas
+pip install pandas
